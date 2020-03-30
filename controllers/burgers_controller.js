d12ca2a74a0fdd95771ec1e9b2fefe751c213c52
Amanda Higgins <amanda.n.higgins@gmail.com>
2020-03-30 18:25:50 -0400

    adding code

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,66 @@
+// Require express
+var express = require("express");
+// Require express
+var express = require("express");
+// Import the burger model 
+var burger = require("../models/burger.js");
+
+// Create all of the routes required for the application
+
+// Get
+
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+        });
+    });
+
+// post 
+
+router.post("/api/burgers", function(req,res) {
+    burger.create([
+        "burgerName", "devoured"
+    ], [
+        req.body.name, req.body.devoured
+    ], function(result) {
+        res.json({ id: result.insertId });
+    });
+   });
+
+    // update 
+
+    router.put("/api/burgers/:id", function(req, res) {
+        var condition = "id = " + req.params.id;
+ 
+        console.log("condition", condition);
+ 
+        burger.update({
+            devoured: req.body.devoured
+        }, condition, function(result) {
+            if (result.changedRows == 0) {
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+            });
+        });
+ 
+     // delete
+ 
+     router.delete("/api/burgers/:id", function(req, res) {
+         var condition = "id = " + req.params.id;
+ 
+         burger.delete(condition, function(result) {
+             if (result.affectedRows == 0) {
+                  return res.status(404).end();
+                 } else {
+                     res.status(200).end();
+                 }
+                 });
+             });
+
+module.exports = router;
